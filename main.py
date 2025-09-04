6db4b84cefb9278c1312593f5e6455fd753e2602
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-09-04 17:47:28 -0300

    listas array, encontrando maior numero

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('Estrutura de Dados e Algoritmos - Python ')
+
+#Listas (Arrays)
+#encontrar o maior número em uma lista
+numeros = [3, 5, 2, 8, 1]
+maior = numeros[0]
+
+for num in numeros:
+    if num > maior:
+        maior = num
+        
+print(f'O maior número é: {maior}')
